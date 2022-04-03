b5e64e3447b4d5ca8ad926510ff1e76cfb88cc47
patrickbuerk <98759563+patrickbuerk@users.noreply.github.com>
2022-04-03 16:12:37 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter your age");
+        int ageOver = 22;
+        int ageUnder =19;
+        if (ageOver >= 21 ){
+            System.out.println("You get a wristband for being 21 or over ");
+        }
+        else ;
+        if(ageUnder >=21){
+            System.out.println("You get a wristband for being 21 or over ");
+
+        }
+        else;
+
+    }
+}
